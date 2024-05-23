d0d5c27755915b8c8b5a4e732fddf6318ae03c8a
Julia Retuerta <elgrankeko@hotmail.com>
2024-05-23 18:51:46 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -133,10 +133,56 @@ multiplication(7);
 //     ....
 
 const ageAndYear = (year, age) => {
-  const nacimiento = year - age;
-  console.log(`naciste en el año ${nacimiento}`);
+  console.log(`naciste en el año ${year - age}`);
 
-  for (let counter = 2024; counter >= 0; counter--) {}
+  for (let counter = 1; counter <= 33; counter++) {
+    if (counter === 1) {
+      console.log(`En el año ${counter + (year - age)} cumpliste ${counter} año`);
+    } else {
+      console.log(`En el año ${counter + (year - age)} cumpliste ${counter} años`);
+    }
+  }
 };
 
 ageAndYear(2024, 33);
+
+// 6 - Crea una función que reciba dos números e imprima todos los números pares desde el primero hasta el segundo. Si recibe 2 y 12 imprimirá 2, 4, 6, 8, 10, 12.
+
+const twoNumbers = (number1, number2) => {
+  let start;
+  let end;
+
+  if (number1 < number2) {
+    start = number1;
+    end = number2;
+  } else {
+    start = number2;
+    end = number1;
+  }
+  for (let counter = start; counter <= end; counter++) {
+    if (counter % 2 === 0) {
+      console.log(counter);
+    }
+  }
+};
+
+twoNumbers(2, 10);
+
+// 7- Crea una función que genere 2 arrays y los rellene con 5 números aleatorios cada uno, la función debe decir qué valores se han repetido en los dos arrays.
+
+const twoArrays = () => {
+  const array1 = [];
+  const array2 = [];
+
+  for (let counter = 0; counter <= 5; counter++) {
+    const number1 = Math.floor(Math.random() * 10);
+    const number2 = Math.floor(Math.random() * 10);
+    array1.push(number1);
+    array2.push(number2);
+  }
+
+  if (number1.include(array1)) {
+  }
+};
+
+twoArrays();
